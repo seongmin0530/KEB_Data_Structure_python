d864abbd867c925aabd9e23e51f236b51a36e3b7
kimseongmin <kpmgksm@naver.com>
2023-02-03 14:26:31 +0900

    Data Structure_Day01_08_Linear List very large polynomial calculation

--- Data_Structure_Day01_00.py
+++ Data_Structure_Day01_00.py
@@ -1,54 +1,43 @@
-# Data Structure_Day01_07
-# 선형 리스트 ==>  다항식의 선형 리스트 표현과 계산 프로그램
+# Data Structure_Day01_08
+# 선형 리스트 ==>  특수 다항식 처리 프로그램
+
+# 차수가 매우 큰 다항식을 처리할 때 for문을 이용해 작성하면
+# 리스트 공간 낭비가 심하므로 새로운 리스트를 만들어 계수가 있는 차수 정보를 저장
 
 
-def print_Poly(px):
-    """
-    다항식 출력 함수
-    :param px: list(계수가 담겨있는 리스트)
-    :return: String(출력할 문자열)
-    """
-    term = len(px) - 1  # 최고차항 숫자 = 배열길이-1
+def print_poly(tx, px):
     poly_str = "P(x) = "
 
     for i in range(len(px)):
+        term = tx[i]  # 항 차수
         coef = px[i]  # 계수
 
-        if coef > 0 and i != 0:
-            poly_str = poly_str + "+"
-
-        elif coef == 0:
-            term = term - 1
-            continue
-
-        poly_str = poly_str +f'{coef}x^{term} '
-        term -= 1
+        if (coef >= 0):
+            poly_str += "+"
+        poly_str += f'{coef}x^{term}'
 
     return poly_str
 
 
-def calc_Poly(x_Val, px):
-    """
-    다항식 계산 함수
-    :param x_Val:int(x값)
-    :param px:inst(계수)
-    :return:int(계산결과)
-    """
-    return_val = 0
-    term = len(px) - 1  # 최고차항 숫자 = 배열길이-1
+def calc_poly(xVal, tx, px):
+    ret_value = 0
 
     for i in range(len(px)):
-        coef = px[i]  # 계수
-        return_val += coef * x_value ** term
-        term -= 1
+        term = tx[i]
+        coef = px[i]
+        ret_value += coef * x_value ** term
 
-    return return_val
+    return ret_value
 
 
-px = [7, -4, 0, 5]
+px = [7, -4, 5]
+tx = [300, 20, 0]
+
 
 if __name__ == "__main__":
-    print(print_Poly(px))
-    x_value = int(input("X 값 : "))
-    print(calc_Poly(x_value, px))
+    print(print_poly(tx, px))
+
+    x_value = int(input("X 값을 입력하세요.: "))
+
+    print(calc_poly(x_value, tx, px))
 
