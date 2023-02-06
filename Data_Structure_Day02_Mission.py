c56713aac08abed6dd75524d51d440c00dfce8ab
kimseongmin <kpmgksm@naver.com>
2023-02-06 23:04:21 +0900

    Data Structure_Day02_mission03

--- Data_Structure_Day02_Mission.py
+++ Data_Structure_Day02_Mission.py
@@ -1,51 +1,71 @@
-class Node():
-    def __init__(self, data):
-        self.data = data
-        self.plink = None   # 정방향 링크
-        self.nlink = None   # 역방향 링크
+# Day02_Mission03
+#마라톤 반환점 돌아오기
+
+import random
 
 
-def print_nodes(start):
-    current = start
-    print("정방향 ==>", end=" ")           # 정방향 출력
-    print(current.data, end=" ")          # 첫번째 노드 출력
-    while current.plink != None:          # 정방향 링크가 None이 아닐 때 까지
-        current = current.plink           # current 한칸 이동 후 출력
-        print(current.data, end=" ")
+def is_stack_full():
+    global stack, top, SIZE
+    if top >= SIZE - 1:
+        # print("스택이 꽉 찼습니다. push를 수행할 수 없습니다.")
+        return True
+    return False
 
-    print("\n역방향 ==>", end=" ")         # 역방향 출력
-    while current.nlink != None:          # 역방향 링크가 None이 아닐 때 까지
-        print(current.data, end=" ")      # 노드 출력 후 current 한칸 이동
-        current = current.nlink
-    print(current.data, end=" ")          # 출력 후 한칸 이동하여 역방향 마지막 노드가 출력되지 않았으므로 따로 출력
 
-    # 동일 기능(print_nodes(start))
-    # ***********************************************
-    # def print_nodes(start):
-        # current = start
-        # if current.plink == None:
-        #     return
-        # print("정방향 ==>", end=" ")
-        # print(current.data, end=" ")
-        # while current.plink != None:
-        #     current = current.plink
-        #     print(current.data, end=" ")
-        # print("\n역방향 ==>", end=" ")
-        # print(current.data, end=" ")
-        # while current.nlink != None:
-        #     current = current.nlink
-        #     print(current.data, end=" ")
-    # ***********************************************
-pre, current, head = None, None, None
-data_array = ["카게야마", "히나타", "츠키시마", "타나카", "다이치"]
+def is_stack_empty():
+    global stack, top, SIZE
+    if top == -1:
+        # print("스택이 텅텅 비었습니다. pop을 수행할 수 없습니다.")
+        return True
+    return False
+
+
+def push(data):
+    global stack, SIZE, top
+    if is_stack_full():
+        return
+    top += 1
+    stack[top] = data
+
+
+def pop():
+    global stack, SIZE, top
+    if is_stack_empty():
+        return
+    data = stack[top]
+    stack[top] = None
+    top = top -1
+    return data
+
+
+def peek():
+    global stack, SIZE, top
+    if is_stack_empty():
+        print("현재 스택에 아무 자료도 없습니다.")
+        return None
+    print(stack[top])
+
+
+SIZE = 10
+stack = [None for i in range(SIZE)]
+top = -1
 
 if __name__ == "__main__" :
-    node = Node(data_array[0])
-    head = node
-    for data in data_array[1:]:
-        pre = node
-        node = Node(data)
-        pre.plink = node        # 기존 노드의 정방향 링크를 새 노드와 연결
-        node.nlink = pre        # 새 노드의 역방향 링크를 기존 노드와 연결
+    spot_array = ["주안역", "학산소극장", "인하대 후문", "인하대 정문", "숭의역", "인하대 송도 캠퍼스"]
+    random.shuffle(spot_array)
+    count = 0
+    print("반환점 가는길 :", end=" ")
+    for spot in spot_array:
+        push(spot)
+        count += 1
+        print(f'{spot} -->',end=" ")
+    print("반환점")
 
-    print_nodes(head)
+    print("시작점 가는길 :", end=" ")
+    while True:
+        spot = pop()
+        if spot:
+            print(f'{spot} -->', end=" ")
+        else:
+            break
+    print("시작점")
