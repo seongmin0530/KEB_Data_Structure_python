1dda261c915e630651bff8ab8f464c21c8caeb2d
kimseongmin <kpmgksm@naver.com>
2023-02-07 17:23:50 +0900

    Day03_mission03

--- Data_Structure_Day03_Mission.py
+++ Data_Structure_Day03_Mission.py
@@ -1,84 +1,74 @@
-# Day03_Mission01
-# 방방 대기시간 use circular queue
-def is_queue_full():
+# Day03_Mission03
+# 도서관 대여도서 목록 출력하기 use binary search tree
+import random
+
+
+class Tree_node():
+    def __init__(self):
+        self.data = None
+        self.llink = None
+        self.rlink = None
+
+
+def preorder(node) :
     """
-    queue의 상태를 확인하는 함수(가득 찼을 때 True 반환)
-    :return: True/False
+    전위 순회 알고리즘
+    :param node: 루트노드
+    :return: void
     """
-    global SIZE, queue, front, rear
-    if (rear+1)%SIZE == front   :                  # rear의 위치가 queue의 끝 인덱스가 아니라면 queue가 꽉 찬 것이 아님
-        return True                               # False 반환
-    else :
-        return False
+    if node == None:
+        return
+    print(node.data, end=' ')      # 1. 현재 노드 처리
+    preorder(node.llink)            # 2. 왼쪽 서브트리 이동 (자기자신 호출) --> 재귀함수
+    preorder(node.rlink)            # 3. 오른쪽 서브트리 이동 (자기자신 호출) --> 재귀함수
 
 
-
-def is_queue_empty():
+def make_tree():
     """
-    queue의 상태를 확인하는 함수(비어있을 때 True 반환)
-    :return: True/False
+    이진트리 생성하는 함수
+    :return: void
     """
-    global SIZE, queue, front, rear
-    if front == rear :          # front의 위치가 rear와 같다면 queqe가 비어있는 것임
-        return True
-    else :
-        return False
+    global root, rental_array
+    node = Tree_node()
+    node.data = rental_array[0]
+    root = node
+
+    for data in rental_array[1:]:
+
+        node = Tree_node()
+        node.data = data
+
+        current = root
+        while True:
+            if data < current.data:
+                if current.llink == None:
+                    current.llink = node
+                    break
+                current = current.llink
+
+            elif data == current.data:
+                break
+
+            else:
+                if current.rlink == None:
+                    current.rlink = node
+                    break
+                current = current.rlink
+    print("\n이진 탐색 트리 구성 완료!\n")
 
 
-def en_queue(data):
-    """
-    queue에 data 삽입하는 함수
-    :param data: 삽입할 데이터
-    :return: True(bool)
-    """
-    global SIZE, queue, front, rear
-    if(is_queue_full()):
-        print("queue가 꽉 찼습니다.")
-        return True
-    rear = (rear+1) % SIZE
-    queue[rear] = data
+root = None
+data_array = ["어린왕자","생각의 탄생","총균쇠","자료구조","python","java","일반수학"]
+rental_array = [random.choice(data_array) for i in range(15)]
 
 
-def de_queue():
-    """
-    queue의 데이터를 삭제하는 함수
-    :return: 삭제할 데이터
-    """
-    global SIZE, queue, front, rear
-    if is_queue_empty() :
-        # print("queue가 텅텅 비어있습니다.")
-        return None
-    front  = (front+1) % SIZE
-    data = queue[front]
-    queue[front] = None
-    return data
+if __name__ == "__main__":
+    print(f'금일 대여 도서 목록 :{rental_array}')
+    make_tree()
+    print(f'금일 대여 도서 명 :',end=" ")
+    preorder(root)
 
 
-def peek() :
-    """
-    queue의 다음에 반환할 data를 찾는 함수
-    :return: 다음에 반환(삭제)할 값
-    """
-    global SIZE, queue, front, rear
-    if is_queue_empty():
-        return None
-    return queue[front + 1]
 
 
-SIZE = 6
-queue = [None for i in range(SIZE)]
-front = 0     # 출구    (초기에 출구는 입구와 같은 위치여야 함)
-rear = 0       # 입구    (-1이어야 처음부터 자료가 들어감)
-total_wait = 0;
 
-if __name__ == "__main__" :
-    data_array = [("최고코스",60), ("중간코스",30), ("간편 코스",15), ("예약안내",10), ("간편코스",15)]
-
-    print(f'예상 대기시간은 {total_wait}분 입니다.')
-    print(f'방방 대기손님 내역 : {queue}\n')
-    for data in data_array:
-        total_wait += data[1]
-        en_queue(data)
-        print(f'예상 대기시간은 {total_wait}분 입니다.')
-        print(f'방방 대기손님 내역 : {queue}\n')
-    print("예상 대기시간 보기를 종료합니다.")
