88828b316b524895e1be087ab4030f1d34783efc
kimseongmin <kpmgksm@naver.com>
2023-02-07 13:01:15 +0900

    Day03_queue_circular queue

--- Da03_queue.py
+++ Da03_queue.py
@@ -1,25 +1,19 @@
-# Day03_ Queue
-# 삽입 ==> rear 이동, 삭제 ==> front 이동
-# queue가 비어있는 상태 ==> front == rear
-# queue size 만큼 삽입, 삭제 후에 front, rear 위치 = SIZE-1 ==> 원형 queue로 해결...?
-# 기본적인 큐 삽입 종료후 여러번 삭제하면 queue가 다 차지 않았으나 코드상 꽉 찼다고 인식하는 경우 발생 ==> is_queue_full 개선으로 해결
+# Day03_ Queue - 원형 queue
+# is_queue_full => elif에서 자료를 옮기는 부분이 자료의 size가 커지면 굉장히 과정이 길어짐 -> 엄청난 오버헤드
+# 원형 queue로 해결
+# 선형 queue와 다르게 0부터 시작
+# 선형 queue와 다르게 % SIZE를 해줘 원형으로 이어줌
+# 단점 --> 한칸의 공간 을 못쓰게 됨 (SIZE-1개의 공간만 사용할 수 있기 때문에) ==> 꽉차게 쓰면 컴퓨터는 queue가 빈 것을 이해함(front == rear이기 때문)
 def is_queue_full():
     """
     queue의 상태를 확인하는 함수(가득 찼을 때 True 반환)
     :return: True/False
     """
     global SIZE, queue, front, rear
-    if rear != SIZE -1 :                        # rear의 위치가 queue의 끝 인덱스가 아니라면 queue가 꽉 찬 것이 아님
-        return False                                # False 반환
-    elif rear == SIZE-1 and front == -1 :        # rear의 위치가 queue의 끝 인덱스이고 front가 -1이라면 queue가 꽉 찬 것임
-        return True                                 # True 반환
-    else :                                      # rear의 위치는 queue의 끝이지만 front가 -1이 아닐 때 (queue가 꽉 찬 척 할 때)
-        for i in range(front+1, SIZE):          # queue에 있는 자료들 한칸씩 이동
-            queue[i-1] = queue[i]
-            queue[i] = None
-        front -= 1                              # queue가 이동했으니 front와 rear도 1칸씩 줄임
-        rear -= 1
-        return False                            # queue가 안찼으니 False 반환
+    if(rear +1)%SIZE == front :         # 변화 : FRONT와 REAR가 같은 경우
+        return True
+    else:
+        return False
 
 
 def is_queue_empty():
@@ -41,10 +35,10 @@ def en_queue(data):
     :return: True(bool)
     """
     global SIZE, queue, front, rear
-    if(is_queue_full()):
+    if is_queue_full() :
         print("queue가 꽉 찼습니다.")
-        return True
-    rear += 1
+        return
+    rear  = (rear+1) % SIZE                 # 변화 : 원형 형태로 이어주기 위해 %SIZE (기존 : raer += 1)
     queue[rear] = data
 
 
@@ -57,7 +51,7 @@ def de_queue():
     if is_queue_empty() :
         print("queue가 텅텅 비어있습니다.")
         return None
-    front += 1
+    front = (front+1) % SIZE                # 변화 : 원형 형태로 이어주기 위해 %SIZE(기존 : FRONT +=1)
     data = queue[front]
     queue[front] = None
     return data
@@ -71,12 +65,21 @@ def peek() :
     global SIZE, queue, front, rear
     if is_queue_empty():
         return None
-    return queue[front + 1]
+    return queue[(front + 1) % SIZE]        # 변화 : 원형 형태로 이어진 자료를 탐색하기 위해 %SIZE(기존 : front + 1)
 
 
-SIZE = int(input("queue 크기를 입력해주세요. : "))
+SIZE = 10
 queue = [None for i in range(SIZE)]
-front = -1      # 출구
+front = 0     # 원형 QUEUE이기 때문에 0부터 시작
 rear = 0
 
 if __name__ == "__main__" :
+    data_array = ["a", "b", "c", "d"]
+    for data in data_array:
+        en_queue(data)
+        print(data, end=" ")
+    print()
+    print(queue)
+
+    for j in range(5) :
+        print(de_queue(), end=" ")
