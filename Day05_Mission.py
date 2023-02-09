1feddff278a7c3e3eaefd18c33c0aef2800f0352
kimseongmin <kpmgksm@naver.com>
2023-02-09 17:35:53 +0900

    Day05. Mission04

--- Day05_Mission.py
+++ Day05_Mission.py
@@ -1,35 +1,44 @@
 # Day05_Mission
-# 교실에 있는 학생중 중간 성적을 가진 학생 찾기
+# 퀼정렬 실행시간 확인
 
-def sort(array):
-    for end in range(len(array)):
-        for j in range(end,0,-1):
-            if array[j-1]>array[j]:
-                array[j-1], array[j] = array[j],array[j-1]
-    sorted_array = array
+import random
+import time
+
+def q_sort(array,start,end):
+    if end <= start:
+        return
+
+    low = start
+    high = end
+
+    pivot = array[(low + high) // 2]            # 1. 중간지점 잡기
+    while low <= high:                  # 2. Low가 High가 같아질 때까지 반복
+        while array[low] < pivot:               # 3-1. Low값이 pivot보다 작을때 까지              # (< 올림차순)/(> 내림차순)
+            low += 1                                # 4-1. Low자리 증가
+        while array[high] > pivot:              # 3-2. High값이 pivot보다 클 때까지               # (> 올림차순)/(<내림차순)
+            high -= 1                               # 4-2. High값 감소
+        if low <= high:                         # 3. low값이 High값보다 작으면
+            array[low], array[high] = array[high], array[low]
+            low,high = low + 1, high - 1            # 4. 해당 위치의 Low/High값을 교환하고 Low값과 High값 증가,감소시킴
+
+    mid = low                                   # 5. Low값을 middle값으로 설정
+
+    q_sort(array, start, mid-1)                 # 6. pivot 기준 왼쪽 배열 sorting by 재귀호출
+    q_sort(array, mid, end)                     # 6. pivot 기준 오른쪽 배열 sorting by 재귀호출
+
+
+def quick_sort(array):                          # 주어진 배열을 이용해 q_sorting 호출하는 최종 함수
+    q_sort(array,0,len(array)-1)
     return array
 
 
-def make_array(array):
-    result_array = list()
-    for i in range(len(array)):
-        for j in range(len(array[i])):
-            result_array.append(array[i][j])
-    return result_array
-
-
-def find_middle(array):
-    print(array[len(array)//2])
-
-
-people_array = [[20,35,40,90],
-                [47,62,79,30],
-                [10,43,72,59],
-                [5,26,65,87]]
-
+amount_data = [10000,50000, 100000, 150000]
 
 if __name__ == "__main__":
 
-   sorted_array = sort(make_array(people_array))
-   print(sorted_array)
-   find_middle(sorted_array)
+    for i in amount_data:
+        data_value = [random.randint(0,20000) for _ in range(i)]
+        start = time.time()
+        quick_sort(data_value)
+        end = time.time()
+        print(f'{i}개의 데이터 퀵정렬 종료. 실행시간 : {end-start:.2f}')
